e082e8e64aae57066d03470e40ebd93073324db9
Jônatas <jonatasthome04@gmail.com>
2025-07-29 14:50:08 -0300

    Started to work on script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Teste!")
+//Elements
+const generatePasswordButton = document.querySelector("#generate-password")
+const generatedPasswordElement = document.querySelector("#generated-password")
+
+//Functions
+const symbols = "@#$%&?!\|><{}[]*()"
+
+function getSymbols (){
+    return symbols[Math.floor(Math.random() * symbols.length)].toString()
+}
+
+console.log(getSymbols())
+
+//Events
+generatePasswordButton.addEventListener("click",()=>{
+    console.log("Teste!")
+})
