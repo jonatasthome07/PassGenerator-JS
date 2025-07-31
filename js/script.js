fddaa945cd472ea43a02220c99d7be81d37c876f
Jônatas <jonatasthome04@gmail.com>
2025-07-30 21:11:48 -0300

    Progressed in script.js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,26 @@ const generatedPasswordElement = document.querySelector("#generated-password")
 //Functions
 const symbols = "@#$%&?!\|><{}[]*()"
 
+function LetterLowerCase(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function LetterUpperCase(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
 function getSymbols (){
     return symbols[Math.floor(Math.random() * symbols.length)].toString()
 }
 
+function getNumber (){
+    return Math.floor(Math.random() * 10).toString()
+}
+
 console.log(getSymbols())
+console.log(LetterLowerCase())
+console.log(LetterUpperCase())
+console.log(getNumber())
 
 //Events
 generatePasswordButton.addEventListener("click",()=>{
