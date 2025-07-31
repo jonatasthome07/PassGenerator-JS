58df8b5eeea123699d6d92bb438d131a930c8348
Jônatas <jonatasthome04@gmail.com>
2025-07-31 00:22:46 -0300

    Function to generate password was added

--- js/script.js
+++ js/script.js
@@ -3,30 +3,42 @@ const generatePasswordButton = document.querySelector("#generate-password")
 const generatedPasswordElement = document.querySelector("#generated-password")
 
 //Functions
-const symbols = "@#$%&?!\|><{}[]*()"
+const symbols = "@#$%&?!\|><{}[]*()-_+=~^'¨'"
 
-function LetterLowerCase(){
+const LetterLowerCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-function LetterUpperCase(){
+const LetterUpperCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
 
-function getSymbols (){
+const getSymbols = () => {
     return symbols[Math.floor(Math.random() * symbols.length)].toString()
 }
 
-function getNumber (){
+const getNumber = () => {
     return Math.floor(Math.random() * 10).toString()
 }
 
-console.log(getSymbols())
-console.log(LetterLowerCase())
-console.log(LetterUpperCase())
-console.log(getNumber())
+const generatePassword = (LetterLowerCase, LetterUpperCase, getSymbols, getNumber) =>{
+    let password = ""
+    const passwordlength = 10
+
+    const generators = [LetterLowerCase, LetterUpperCase, getSymbols, getNumber]
+
+    for (i = 0; i < passwordlength; i = i + generators.length) {
+        generators.forEach(()=>{
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]()
+            password += randomValue
+        })
+    }
+    password = password.slice(0, passwordlength)
+    generatedPasswordElement.style.display = "block"
+    generatedPasswordElement.querySelector("h4").innerText = password
+}
 
 //Events
 generatePasswordButton.addEventListener("click",()=>{
-    console.log("Teste!")
+    generatePassword(LetterLowerCase, LetterUpperCase, getSymbols, getNumber)
 })
